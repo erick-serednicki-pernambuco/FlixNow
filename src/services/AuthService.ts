add20fdb15280c042aec029ea988ff9127dc23a0
unknown <2214290037>
2023-06-29 22:03:12 -0300

    tentativa do register

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,22 +1,29 @@
-import { getAuth, signInWithEmailAndPassword, signOut, Auth } from 'firebase/auth';
-import { app } from './FirebaseConfig';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { app } from './FirebaseConfig'
 
-const auth: Auth = getAuth(app);
+const auth = getAuth(app)
 
-export async function login( email: string, senha: string ): Promise<string> {
-  const userId  = await signInWithEmailAndPassword(auth, email, senha)
+export async function login(email, senha) {
+  return await signInWithEmailAndPassword(auth, email, senha)
     .then((userCredential) => userCredential.user.uid)
     .catch((error) => {
-      if (error.code == 'auth/user-not-found') {
-        throw Error('Usuário não encontrado');
-      } else if (error.code == 'auth/wrong-password') {
-        throw Error('Senha inválida');
+      if (error.code === 'auth/user-not-found') {
+        throw new Error('Usuário não encontrado')
+      } else if (error.code === 'auth/wrong-password') {
+        throw new Error('Senha inválida')
       }
-    });
-
-    return `$userId`
+    })
 }
 
-export async function logout(): Promise<void> {
-  await signOut(auth);
+export async function logout() {
+  await signOut(auth)
+}
+
+export async function createUser(email, senha) {
+  return await createUserWithEmailAndPassword(auth, email, senha)
+    .then((userCredential) => userCredential.user.uid)
+    .catch((error) => {
+      // Lidar com erros de criação de usuário, se necessário
+      throw new Error('Erro ao criar usuário: ' + error.message)
+    })
 }
